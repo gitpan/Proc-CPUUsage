ca5df5eda968d660e0983f9111e9cdc5060975cf
Pedro Melo <melo@cpan.org>
2009-10-10 12:49:44 +0000

    Import of MELO/Proc-CPUUsage-1.001 from CPAN.
    gitpan-cpan-distribution: Proc-CPUUsage
    gitpan-cpan-version:      1.001
    gitpan-cpan-path:         MELO/Proc-CPUUsage-1.001.tar.gz
    gitpan-cpan-author:       MELO
    gitpan-cpan-maturity:     released

--- lib/Proc/CPUUsage.pm
+++ lib/Proc/CPUUsage.pm
@@ -1,5 +1,5 @@
 package Proc::CPUUsage;
-our $VERSION = '1.0';
+our $VERSION = '1.001';
 
 
 
@@ -50,7 +50,7 @@ Proc::CPUUsage - measures the percentage of CPU the current process is using
 
 =head1 VERSION
 
-version 1.0
+version 1.001
 
 =head1 SYNOPSIS
 
